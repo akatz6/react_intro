8b1a7ece53c955c505ac3415507cf32813f50bb0
akatz6 <aaronk488@gmail.com>
2023-06-10 17:30:18 -0700

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,53 @@
 import { useState } from "react";
 import { movies } from "./movies.js";
 import DisplayMovies from "./displayMovies.jsx";
+/*
+  To add react bootstrap you need to 
+  add regular bootstrap and react bootstrap
+  npm install bootstrap && npm install react-bootstrap bootstrap
+  Then only add the css file to your main.jsx file
+  Finally your need to add the component to the page,
+  this save space so you only add the component and not the entire library
+ */
+//
 import Button from "react-bootstrap/Button";
 import "./App.css";
 
 function App() {
+  // Add JS here
   const [count, setCount] = useState(0);
-  const [movies2, setMovies] = useState(movies);
+  const [countWithParam, setCountWithParam] = useState(0);
 
-  const click = (x) => {
-    setCount(count + x);
+  const addValue = () => {
+    setCount(count + 1);
   };
 
+  const addValueWithParam = (x) => {
+    setCountWithParam(countWithParam + x);
+  };
+
+  console.log(movies);
+
+  // Combination of html and js goes into return statement
   return (
     <>
-      <Button variant="primary" onClick={() => click(2)}>
-        count is {count}
-      </Button>
+      <div className="buttons">
+        <Button onClick={addValue}>The count is: {count}</Button>
+        {/* we use () => function() so we don't get an infinite loop */}
+        <Button variant="success" onClick={() => addValueWithParam(2)}>
+          The count is: {countWithParam}
+        </Button>
+      </div>
       <br />
       <br />
       <br />
       <div className="container">
-        {movies2.map((movie) => (
+        {movies.map((movie) => (
           <DisplayMovies
             key={movie.Title}
-            title={movie.Title}
             image={movie.Images[0]}
             rated={movie.Rated}
+            title={movie.Title}
           />
         ))}
       </div>
--- src/displayMovies.jsx
+++ src/displayMovies.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+// props passes in the parameters from the parent
 function DisplayMovies(props) {
   return (
     <div className="element">
